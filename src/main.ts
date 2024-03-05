fa983d953cb5e88b4d5027ee3b574d0a236ac70b
VladOvOl <v.ovsanik2002@gmail.com>
2024-03-05 14:22:24 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+		origin: ['http://localhost:5555'],
+		credentials: true,
+		exposedHeaders: 'set-cookie'
+	})
   await app.listen(3000);
 }
 bootstrap();
